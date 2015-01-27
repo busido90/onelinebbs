9216b52e55671740a80b13a899d6e785816d81e8
busido90 <nagatai0120@gmail.com>
2015-01-28 00:25:53 +0800

    add bbs.php

---
+++ bbs.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>ひとこと掲示板</title>
+<style>
+</style>
+<!-- <link rel="stylesheet" href="style.css"> -->
+</head>
+<body>
+	<h1>ひとこと掲示板</h1>
+
+	<form action="bbs.php" method="post">
+		名前：　<input type="text" name="name" /><br />
+		ひとこと：　<input type="text" name="comment" size="60" /><br />
+		<input type="submit" name="submit" value="送信" />
+	</form>
+</body>
+</html>
